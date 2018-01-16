ce8eb2d9e66b5075f24ed87e11a01ade7e8550d3
noah-Hong <cult4368@gmail.com>
2018-01-16 15:38:31 +0900

    init projects

--- testApp103/FirstViewController.swift
+++ testApp103/FirstViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,24 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    var list = ["My Info", "Setting", "General"]
 
 }
+
+
+extension FirstViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        cell.textLabel?.text = "\(list[indexPath.row])"
+        return cell
+    }
+    
+    
+}
+
 
--- testApp103/SecondViewController.swift
+++ testApp103/SecondViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    var dict = [String: Int]()
+    var keys: [String]?
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if let list = keys {
+            return list.count
+        } else {
+            return 0
+        }
+        //위와 같은 의미
+        //return keys?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell2 = tableView.dequeueReusableCell(withIdentifier: "cell2")!
+        
+        let targetKey = keys?[indexPath.row]
+        let targetValue = dict[targetKey]
+        cell2.textLabel?.text = targetValue
+        
+        return cell2
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
